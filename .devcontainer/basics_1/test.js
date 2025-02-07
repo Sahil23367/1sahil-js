573305c535cbbf90e914c731950d0478fc55719e
Sahil23367 <sahilkumar762300@gmail.com>
2025-02-07 15:29:52 +0000

    helo

--- .devcontainer/basics_1/test.js
+++ .devcontainer/basics_1/test.js
@@ -1,1 +1,5 @@
 console.log("sahil")
+let obj1 = { name: "Alice" };
+let obj2 = obj1; // obj2 points to the same memory location as obj1
+obj2.name = "Bob";
+console.log(obj1.name); // "Bob" (changed)
