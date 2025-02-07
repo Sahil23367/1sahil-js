9307bbee1ef1b5b9ab1afc16e92a748515206fe1
Sahil23367 <sahilkumar762300@gmail.com>
2025-02-07 18:28:17 +0000

    helo

--- .devcontainer/basics_1/test.js
+++ .devcontainer/basics_1/test.js
@@ -1,5 +1,57 @@
-console.log("sahil")
-let obj1 = { name: "Alice" };
-let obj2 = obj1; // obj2 points to the same memory location as obj1
-obj2.name = "Bob";
-console.log(obj1.name); // "Bob" (changed)
+// let a = true;
+// let b = true;
+// console.log(a && b); // false
+
+
+// let age = 20;
+// if (age >= 18 && age < 60) {
+//   console.log("You are an adult.");
+// } 
+// Output: "You are an adult."
+
+
+//logial or in js
+
+// let a = false;
+// let b = false;
+// console.log(a || b); // true
+
+
+// if else statment in js
+
+// let age= 70;
+// if (age>80){
+//     console.log("you are senior")
+// }
+// else if(age>75){
+//     console.log("you are middle ager")
+// }
+// else if(age===71){
+//     console.log("yes you are good")
+// }
+// else(
+//     console.log("bye")
+// )
+
+
+// ternary operator
+
+// let age =24;
+// let result = age>=25? "adult": "you are not adult";
+// console.log(result)
+
+// let str = "   Hello, World!   ";
+// console.log(str);
+
+// let trimmedStr = str.trim();
+// console.log( str.trim()); 
+// let text = "";
+
+// console.log(text.charAt(0));  // Output: "J"
+// console.log(text.charAt(4));  // Output: "S"
+// console.log(text.charAt(20)); // Output: "" (empty string)
+
+let text = "Hello, world!";
+console.log(text.indexOf("world"));  // Output: 7
+console.log(text.indexOf("o"));      // Output: 4 (first occurrence of "o")
+console.log(text.indexOf("xyz"));  //Output: -1 (not found)// or false in this statment in javascript
